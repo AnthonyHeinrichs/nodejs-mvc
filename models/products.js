870234de8d340d5ef86bff62b8762a1159c16f27
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-14 11:47:53 +0200

    MVC app done, but broken

--- models/products.js
+++ models/products.js
@@ -1,18 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      return cb([])
+    }
+    cb(JSON.parse(data))
+  })
+}
+
 module.exports = class Product {
   constructor(title) {
     this.title = title
   }
 
   save() {
-    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-    fs.readFile(p, (err, data) => {
-      let products = []
-      if (!err) {
-        products = JSON.parse(data)
-      }
+    getProductsFromFile(products => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
@@ -21,12 +27,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([])
-      }
-      cb(JSON.parse(data))
-    })
+    getProductsFromFile(cb)
   }
 }
