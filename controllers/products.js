1076f8cf073d3d8661e6bfafa7041a7ddac8af3b
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-14 11:35:57 +0200

    Storing data in local file

--- controllers/products.js
+++ controllers/products.js
@@ -17,14 +17,14 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll()
-  const prods = products;
-  res.render('shop', {
-    prods: prods,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: prods.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
+  const products = Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  })
 }
--- models/products.js
+++ models/products.js
@@ -1,4 +1,5 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +7,26 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    fs.readFile(p, (err, data) => {
+      let products = []
+      if (!err) {
+        products = JSON.parse(data)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        cb([])
+      }
+      cb(JSON.parse(data))
+    })
   }
 }
