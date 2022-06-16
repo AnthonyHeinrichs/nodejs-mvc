f6bc6602dfc874290362d681dd3e217f7bb29ea4
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-16 15:12:44 +0200

    Show existing values when editing & hookup edit button

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/products')
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 }
 
@@ -17,7 +15,26 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description
   const product = new Product(title, imageUrl, description, price)
   product.save()
-  res.redirect('/');
+  res.redirect('/')
+}
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+  if (!editMode) {
+    return res.redirect('/')
+  }
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    })
+  })
 }
 
 exports.getProducts = (req, res, next) => {
